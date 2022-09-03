223ec171aab798b20dfb25cfb63532d7b7888f0d
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-09-03 19:43:15 +0400

    Task4 is finished, but the result is inappropriate

--- Seminar1/Task4.py
+++ Seminar1/Task4.py
@@ -1,1 +1,22 @@
 #Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+##Функции
+
+#def f(x):
+#    if x ==1:
+#        return 'Целое'
+#    elif x == 2.3:
+#        return 23
+#    else:
+#        return
+    
+#arg = 2
+#print(f(arg))
+#print(type(f(arg)))
+
+import math
+x1 = float(input('Введите координаты X1. \n x1 = '))
+x2 = float(input('Введите координаты X2. \n x1 = '))
+y1 = float(input('Введите координаты Y1. \n y1 = '))
+y2 = float(input('Введите координаты Y2. \n y2 = '))
+distance = math.sqrt( (x1-x2)**2 + (y1-y2)**2 )
+print(distance)
