850c915846650772fec92e682253f2bc5ebc4793
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-09-03 20:28:12 +0400

    Task3 is done

--- Seminar1/Task3.py
+++ Seminar1/Task3.py
@@ -1,1 +1,14 @@
 #Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+print("Задача 3. \nНапишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).", end=" ")
+
+InputCoordinate = int(input ("\n Enter the number of the quarter: "))
+if InputCoordinate == 0:
+    print("Ноль не подходит. Введите число не равное нулю")
+if InputCoordinate == 1:
+    print("x > 0 \ny > 0")
+if InputCoordinate == 2:
+    print("x < 0 \ny > 0")
+if InputCoordinate == 3:
+    print("x < 0 \ny < 0")
+if InputCoordinate == 4:
+    print("x > 0 \ny < 0")
