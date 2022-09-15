f755d0d9ec1d1696f47c15a1ecf0319313f27ad1
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-09-15 18:28:06 +0400

    Task 1 is dome

---
+++ Seminar3/Task1.py
@@ -0,0 +1,29 @@
+# Задайте список из нескольких чисел. 
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+import random
+import math
+
+def EnterCheckNumber():
+    while True:
+        try:
+            n = int(input())
+            break
+        except:
+            continue
+    return n
+
+def Element(some_list, n):
+    for i in range(n):
+        some_list.append(random.randint(0,30))
+
+print("Enter the amout of elements of the list:")
+n1 = EnterCheckNumber()
+list1 = []
+Element(list1, n1)
+print(list1)
+Sum1 = 0
+for i in range(len(list1)):
+    if i%2 != 0:
+        Sum1 += list1[i]
+print('Sum =', Sum1)
