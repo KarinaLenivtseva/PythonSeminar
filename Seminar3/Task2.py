e21288750cc1dfc23634c70c3a8b3be77b3a6d24
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-09-15 18:44:24 +0400

    Task is debuged

--- Seminar3/Task2.py
+++ Seminar3/Task2.py
@@ -6,17 +6,17 @@ from Task1 import Element
 from Task1 import EnterCheckNumber
 
 print("Enter the amout of elements of the list::")
-n2 = EnterCheckNumber()
-list2 = []
-Element(list2, n2)
-print(list2)
-list2_new = []
-while len(list2) != 0:
-    if len(list2) == 1:
-        list2_new.append(list2[0]**2)
+n = EnterCheckNumber()
+list = []
+Element(list, n)
+print(list)
+list_new = []
+while len(list) != 0:
+    if len(list) == 1:
+        list_new.append(list[0]**2)
         break
     else:
-        a2 = list2.pop(0)
-        b2 = list2.pop()
-        list2_new.append(a2*b2)
-print(list2_new)
+        a = list.pop(0)
+        b = list.pop()
+        list_new.append(a*b)
+print(list_new)
