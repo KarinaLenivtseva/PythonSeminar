80686602fd0fb1fe5f9a2a1b05247c3da2938688
Karina Lenivtseva <solovyevakarina@yandex.ru>
2022-09-19 19:05:15 +0400

    Task 4 finished

---
+++ Seminar4/Task4.py
@@ -0,0 +1,36 @@
+# Задана натуральная степень k. 
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и 
+# записать в файл многочлен степени k.
+
+def EnterCheckNumber():
+    while True:
+        try:
+            n = int(input())
+            break
+        except:
+            continue
+    return n
+
+import random
+
+print("k = ")
+k4 = EnterCheckNumber()
+for i in range(k4, -1, -1):
+    file = open('text.txt', 'a', encoding='utf-8')
+    if i == k4:
+        k = random.randint(1,100)
+        file.write(f'{k} * x^{i} ')
+    if k4 > i > 1:
+        k = random.randint(0, 100)
+        if k != 0:
+            file.write(f'+ {k} * x^{i} ')
+    if i == 1:
+        if k != 0:
+            file.write(f'+ {k} * x ')
+    if i == 0:
+        k = random.randint(0, 100)
+        if k != 0:
+            file.write(f'+ {k} = 0')
+        if k == 0:
+            file.write(f'= 0')
+    file.close()
